5d0880d51e1a055ff49f374f9ae7f1a28eedbfdc
Artur0705 <artlil420@gmail.com>
2022-07-28 16:57:58 +1000

    Added functions and used file system to generate HTML page

---
+++ index.js
@@ -0,0 +1,35 @@
+// Packages needed for this application
+const fs = require("fs");
+
+// Html template generator
+const generateHtmlTemplate = require("./src/generateHTML");
+
+// Actions
+const { addManager, addEmployee } = require("./src/actions");
+
+// Function to create html file
+function createToFile(data) {
+    fs.writeFile("./dist/index.html", data, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Youer team profile has bean generated");
+      }
+    });
+  }
+
+// Initialize app
+function init() {
+    addManager()
+      .then(addEmployee)
+      .then((teamProfileData) => {
+        createToFile(generateHtmlTemplate(teamProfileData));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
+  // Function call to initialize app
+  init();
+  
